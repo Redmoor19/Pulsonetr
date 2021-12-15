9e56c0b646c2ae8aedcf7e595f554bfcd6f117f6
Danila Mandrabura <kald03@yandex.ru>
2021-12-15 18:31:07 +0200

    feedback section

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -31,21 +31,18 @@ document.querySelector('.next').addEventListener('click', function () {
         .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
     });
 
-    $('.catalog-item__link').each(function(i) {
-      $(this).on('click', function (e) {
-          e.preventDefault();
-          $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
-          $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active')
-        })
-    })
+    const toggleClass = function (item) {
+      $(item).each(function(i) {
+        $(this).on('click', function (e) {
+            e.preventDefault();
+            $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+            $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active')
+          })
+      })
+    };
 
-    $('.catalog-item__back').each(function(i) {
-      $(this).on('click', function (e) {
-          e.preventDefault();
-          $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
-          $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active')
-        })
-    })
+    toggleClass('.catalog-item__link');
+    toggleClass('.catalog-item__back');
     
   });
 })(jQuery);
